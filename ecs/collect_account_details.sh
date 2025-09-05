3d6ff3f523ed2c2617f3060fe89a97a7ba143d84
Ravi Y <yevanth@amazon.com>
2025-09-04 18:35:45 -0700

    disabling pagination for AWS CLI command output

--- ecs/collect_account_details.sh
+++ ecs/collect_account_details.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 export ACCOUNT_ID=`aws sts get-caller-identity --query "Account" --output text`
+
+#Disable AWS CLI command response pagination
+export AWS_PAGER=''
+
 chmod a+x $GIT_BASE_DIR/account_details.sh
 echo "export ACCOUNT_ID=$ACCOUNT_ID" >> $GIT_BASE_DIR/account_details.sh
+echo "export AWS_PAGER=''" >> $GIT_BASE_DIR/account_details.sh
 
 read -p "Enter the VPC ID to deploy AppMesh, VPC Lattice and ALB resources: " VPC_ID
 echo "export VPC_ID=$VPC_ID" >> $GIT_BASE_DIR/account_details.sh
